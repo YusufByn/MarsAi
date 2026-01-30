a45608ca0e5f4d326805ef97eca5837045b624f0
YusufByn <yusuf.buyukaydin33@gmail.com>
2026-01-30 11:23:35 +0100

    removed 2 functions in the sanitaze files, only kept anti xss function

--- backend/src/utils/secure/sanitize.util.js
+++ backend/src/utils/secure/sanitize.util.js
@@ -5,15 +5,3 @@ export const sanitizeString = (value) => {
     if (typeof value !== "string") return value;
     return value.replace(/</g, "&lt;").replace(/>/g, "&gt;");
   };
-  
-// fonction pour trimmer une string
-export const normalizeString = (value) => {
-    if (typeof value !== "string") return value;
-    return value.trim();
-};
-  
-// fonction pour normaliser une email et la mettre en minuscule
-export const normalizeEmail = (value) => {
-    if (typeof value !== "string") return value;
-    return value.trim().toLowerCase();
-};
