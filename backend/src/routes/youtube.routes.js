5ec52d3ae4290f8cf18b67a69ae77b62dedd596e
Aseroxx <sfr.assistance.fr@gmail.com>
2026-02-12 11:39:43 +0100

    fix 'restricTo' routes using still old func

--- backend/src/routes/youtube.routes.js
+++ backend/src/routes/youtube.routes.js
@@ -1,9 +1,12 @@
 import { Router } from 'express';
 import { uploadToYoutube } from '../controllers/youtube.controller.js';
-import { requireRole } from '../middlewares/requireRole.middleware.js';
-import { checkAuth } from '../middlewares/auth.middleware.js';
+import { checkAuth, restrictTo } from '../middlewares/auth.middleware.js';
 const router = Router();
 
-router.post('/upload', checkAuth, requireRole('admin', 'superadmin'), uploadToYoutube);
+router.use(checkAuth);
+router.use(restrictTo('admin', 'superadmin'));
+
+
+router.post('/upload', uploadToYoutube);
 
 export default router;
