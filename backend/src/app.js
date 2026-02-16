acacee3d89ab87261c241f43ba19f81402d6705b
YusufByn <yusuf.buyukaydin33@gmail.com>
2026-02-16 16:56:02 +0100

    lil fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -33,7 +33,7 @@ app.use(compression());
 app.use(express.urlencoded({ extended: true }));
 
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
 
 // toutes les routes
 app.use('/api', routes);
